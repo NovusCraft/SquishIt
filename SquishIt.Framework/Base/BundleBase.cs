8926a256b01e996dc6cbb9f21c1ad4cd1a688592
Alex Ullrich <alex.ullrich@gmail.com>
2012-02-07 11:00:25 +0800

    - marking Add(params) method on BundleBase obsolete - changing parameter name for Add to make it more clear that folders can be added

--- SquishIt.Framework/Base/BundleBase.cs
+++ SquishIt.Framework/Base/BundleBase.cs
@@ -201,6 +201,7 @@ namespace SquishIt.Framework.Base
             return (T)this;
         }
 
+        [Obsolete]
         public T Add(params string[] filesPath)
         {
             foreach (var filePath in filesPath)
@@ -209,9 +210,9 @@ namespace SquishIt.Framework.Base
             return (T)this;
         }
 
-        public T Add(string filePath)
+        public T Add(string fileOrFolderPath)
         {
-            AddAsset(new Asset(filePath));
+            AddAsset(new Asset(fileOrFolderPath));
             return (T)this;
         }
 
